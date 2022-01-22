2db695a91672db2bcb1639f7ff5ea6bd1d96f5a5
KajalSinghKS <75270002+KajalSinghKS@users.noreply.github.com>
2022-01-23 00:24:49 +0530

    Time: 3 ms (24.09%), Space: 6.2 MB (96.07%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,22 +1,26 @@
-#include<stack>
+
 class Solution {
 public:
     bool isValid(string s) {
         stack<char>mystack;
         
         for(int i=0;i<s.length();i++)
-        {   if(mystack.empty())
+        {   if(s[i]=='('||s[i]=='{'||s[i]=='[')
             mystack.push(s[i]);
-            else if(mystack.top()=='['&&s[i]==']'||mystack.top()=='('&&s[i]==')'||
-            mystack.top()=='{'&& s[i]=='}')
+            
+            else 
             {
-               mystack.pop();
+                if(mystack.empty() || mystack.top()!='['&&s[i]==']'||mystack.top()!='('&&s[i]==')'||
+                mystack.top()!='{'&& s[i]=='}')
+                {
+                    return false;
+                   
+                }
+                mystack.pop();
             }
-            else
-            mystack.push(s[i]);
+            
         }
-        if(mystack.empty())
-            return true;
-        else return false;
+        return mystack.empty();
     }
+    
 };
